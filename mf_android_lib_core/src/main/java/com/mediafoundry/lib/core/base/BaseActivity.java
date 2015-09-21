2f6ba5736c9e4c883136520f6929bb70954701fc
tienduyle <tienl@hostworks.com.au>
2015-09-21 16:19:12 +0930

    Initial Commit

---
+++ mf_android_lib_core/src/main/java/com/mediafoundry/lib/core/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.mediafoundry.lib.core.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tienl on 21/09/2015.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private static final String TAG = BaseActivity.class.getSimpleName();
+
+
+}
